a42387c652b16326cdda816763c3438f0ac5ff61
Rafa Pinheiro <rafaela.spinheiros@gmail.com>
2026-01-14 21:18:19 -0300

    Gerando mensagens com a IA Generativa

--- Python/desafio_etl.py
+++ Python/desafio_etl.py
@@ -36,3 +36,20 @@ users = [user for id in user_ids if (user := get_user(id)) is not None]
 print(f"Total de usuários obtidos: {len(users)}")
 print(json.dumps(users, indent=2, ensure_ascii=False))
 
+# --- Gerando mensagens com OpenAI ---
+def generate_ai_news(user):
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "Você é um expert em marketing bancário."},
+      {"role": "user", "content": f"Crie uma mensagem para {user['name']} sobre a importância dos investimentos (máximo de 100 caracteres)"}
+    ]
+  )
+  return completion.choices[0].message.content.strip('\"')
+
+for user in users:
+  news = generate_ai_news(user)
+  print(news)
+  user['news'].append({"icon": "https://digitalinnovationone.github.io/santander-dev-week-2023-api/icons/credit.svg",
+      "description": news})
+
